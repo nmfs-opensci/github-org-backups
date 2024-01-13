017708392c83e90f826034ead2955eb065b927eb
Eli Holmes - NOAA <eli.holmes@noaa.gov>
2024-01-12 16:43:58 -0800

    Create nmfs-opensci-backup.sh

---
+++ nmfs-opensci-backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# to run this script, 
+# use the command: `bash backup.sh` at the terminal
+
+#
+# SYSTEM VARIABLES
+#
+# The name of your github organization
+ORG_NAME="nmfs-opensci"
+
+# A personal access token for your GH org
+# that has the `read:user` and `read:org` scopes
+ACCESS_TOKEN="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"
+
+# The API version to use
+API_VERSION="2022-11-28"
+
+# The base URL
+BASE_URL="https://api.github.com/orgs"
+
+# Get the current date in the format MMDDYY
+TODAY=$(date +%Y%m%d)
+
+LOG="githubBackup/githubBackupLog.log"
+
+
+#
+# FUNCTIONS
+#
+# Function to backup a repository
